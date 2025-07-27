ef788facbeb24d86725b9b4df8ae8247098adff7
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-07-27 19:17:46 +0430

    adding post operation with api

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,46 @@
+const moment = require("moment/moment");
 const db = require("../connect");
+const jwt = require("jsonwebtoken");
 
 exports.getPosts = (req, res) => {
-    const q = `SELECT p.*, u.id AS userId,name, profilePic FROM posts AS p JOIN users AS u ON (u.id=p.userId)`
-    db.query(q, (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json(data);
+
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("NOT logged in! please login first");
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(401).json("Not valid token");
+        
+        const q = `SELECT p.*, u.id AS userId,name, profilePic FROM posts AS p JOIN users AS u ON (u.id=p.userId) 
+        LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId=? OR p.userId=?
+        ORDER BY p.createdAt DESC`
+        db.query(q,[userInfo.id,userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+        })
     })
 }
+exports.addPost = (req, res) => {
+
+    const token = req.cookies.accessToken;
+    
+    if (!token) return res.status(401).json("NOT logged in! please login first");
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(401).json("Not valid token");
+        
+        const q = "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUE(?)";
+
+        const value = [
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH-mm-ss"),
+            userInfo.id
+        ]
+        db.query(q,[value], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json("post has been created");
+        })
+    })
+}
+
